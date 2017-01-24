de85d847de3c860b1143835769409d7318898fe0
Matthew Gmur <gmattgmur@gmail.com>
2017-01-24 11:36:54 -0800

    add exercise 2

---
+++ GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+	public static void main( String[] args ) {
+		System.out.println( "+------------------------+" );
+		System.out.println( "|                        |" );
+		System.out.println( "| Portland Gas Mart Plus |" );
+		System.out.println( "|                        |" );
+		System.out.println( "| 2017-01-23  04:38PM    |" );
+		System.out.println( "|                        |" );
+		System.out.println( "| Gallons:       10.870  |" );
+		System.out.println( "| Price/gallon: $ 2.089  |" );
+		System.out.println( "|                        |" );
+		System.out.println( "| Fuel total:  $ 22.71   |" );
+		System.out.println( "|                        |" );
+		System.out.println( "+------------------------+" );
+	}
+}
